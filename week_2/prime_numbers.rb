8f2fd6abd178a19add5cf9de89757fba8436e938
ritapragya-biswas <118459973+ritapragya-biswas@users.noreply.github.com>
2022-12-30 17:48:15 +0530

    completing the functions

--- week_2/prime_numbers.rb
+++ week_2/prime_numbers.rb
@@ -1,18 +1,20 @@
-# A prime number is a whole number greater than 1
-# that cannot be exactly divided by any whole number 
-# other than itself and 1 
-# (e.g. 2, 3, 5, 7, 11).
+def prime(n)
 
-# The function below takes a keyword arguments `n` and 
-# returns an array of prime numbers less than or equal to
-# `n`.
+    for i in (2..n/2)
+        if n % i == 0
+            return false
+        end
+    end
+    return true
 
-# For example, prime_numbers(n: 20) should return the following:
-# [2, 3, 5, 7, 11, 13, 17, 19]
+end
 
-# If the user gives a invalid input like -4
-# We will raise an `ArgumentError` exception to let the caller know that
-# their function arguments were incorrect.
 def prime_numbers(n:)
-  raise NotImplementedError # TODO
+    arr = Array.new
+    for i in (2..n)
+        if prime(i)
+            arr.push(i)
+        end
+    end
+    print arr
 end
