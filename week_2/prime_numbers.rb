976f488462a998dfa62574f481a829026f4c8b28
Sharath Balaji T <sharathbalaji2003@gmail.com>
2022-12-30 22:33:23 +0530

    code formatted

--- week_2/prime_numbers.rb
+++ week_2/prime_numbers.rb
@@ -2,23 +2,23 @@
 def prime_numbers(n:)
   prime_num =[]          #Array to store prime numbers
   if n>=2                 #Prime numbers can't be less than 2
-    for i in 2..n        
-      count=0
-      j=i
-      (2..(j-1)).each do |j|
-        if i%j==0
-          count += 1
-        end
-        if count >0
-          break
-        end
-      end
-     if count==0
-     prime_num.push(i)                          #storing prime numbers in the array prime_num
-     end
-  end
-  else
-   raise ArgumentError.new("Incorrect Argument")         # error for n<2
-  end
+     for i in 2..n        
+       count=0
+       j=i
+           (2..(j-1)).each do |j|
+               if i%j==0
+                 count += 1
+               end
+               if count >0
+                 break
+               end
+            end
+       if count==0
+         prime_num.push(i)                          #storing prime numbers in the array prime_num
+       end
+    end
+   else
+     raise ArgumentError.new("Incorrect Argument")         # error for n<2
+   end
  return prime_num
 end
