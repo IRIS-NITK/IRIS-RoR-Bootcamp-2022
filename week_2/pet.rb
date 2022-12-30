84dca2e6408cd5c2c242dee885d6f017b5725afb
Sharath Balaji T <sharathbalaji2003@gmail.com>
2022-12-30 14:56:52 +0530

    Uploaded

--- week_2/pet.rb
+++ week_2/pet.rb
@@ -46,7 +46,12 @@ class Pet
 
   # Return the habitat of the pet
   def habitat
-    raise NotImplementedError # TODO
+
+     for (keys,value) in HABITATS
+      if value.include?(animal_type_id)
+      return  keys
+      end
+    end
   end
 
   # Returns the cost of food required to feed the animal 
@@ -66,7 +71,7 @@ class Pet
   # cat = Pet.new(name: 'cat', animal_type_id: 6, food_consumed_per_day: 0.4)
   # cat.food_required(28) = 11.2 (0.4 * 28)
   def food_required(days)
-    raise NotImplementedError # TODO
+    return food_consumed_per_day*days
   end
 
   # This function takes the number of `days` as the input
@@ -76,7 +81,7 @@ class Pet
   # cat = Pet.new(name: 'cat', animal_type_id: 6, food_consumed_per_day: 0.4)
   # cat.food_cost(28) = 8960
   def food_cost(days)
-    raise NotImplementedError # TODO
+    return food_cost_per_day*days
   end
 
   # This function takes an array of pets and the `days`
@@ -90,7 +95,11 @@ class Pet
   # snake = Pet.new(name: 'python', animal_type_id: 4, food_consumed_per_day: 0.3)
   # Pet.cost_to_feed([cat, dog, fish, snake], 6) will return 6180.0
   def self.cost_to_feed(pets, days)
-    raise NotImplementedError # TODO
+    ttpd=0
+    for pet in pets
+        ttpd+=pet.food_cost(days)
+     end
+    return ttpd
   end
 
   # This function takes an array of pets as input
@@ -110,6 +119,13 @@ class Pet
   #
   # Note - Order is not important
   def self.group_by_animal_type(pets)
-    raise NotImplementedError # TODO
+    grp = {}
+    pets.group_by { |pet| pet.animal_type_id }.each do |animal_type_id, pets|
+    names = []
+    pets.each { |pet| names << pet.name }
+    grp[animal_type_id] = names
   end
+   return grp
+   end
+ 
 end
--- week_2/prime_numbers.rb
+++ week_2/prime_numbers.rb
@@ -1,18 +1,24 @@
-# A prime number is a whole number greater than 1
-# that cannot be exactly divided by any whole number 
-# other than itself and 1 
-# (e.g. 2, 3, 5, 7, 11).
 
-# The function below takes a keyword arguments `n` and 
-# returns an array of prime numbers less than or equal to
-# `n`.
-
-# For example, prime_numbers(n: 20) should return the following:
-# [2, 3, 5, 7, 11, 13, 17, 19]
-
-# If the user gives a invalid input like -4
-# We will raise an `ArgumentError` exception to let the caller know that
-# their function arguments were incorrect.
 def prime_numbers(n:)
-  raise NotImplementedError # TODO
+  prime_num =[]          #Array to store prime numbers
+  if n>=2                 #Prime numbers can't be less than 2
+    for i in 2..n        
+      count=0
+      j=i
+      (2..(j-1)).each do |j|
+        if i%j==0
+          count += 1
+        end
+        if count >0
+          break
+        end
+      end
+   if count==0
+    prime_num.push(i)                          #storing prime numbers in the array prime_num
+   end
+  end
+else
+  raise ArgumentError.new("Incorrect Argument")         # error for n<2
+  end
+    return prime_num
 end
