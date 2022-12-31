6315288c91c92caa0e18404d845f99958b806334
Prashant-3103 <98896437+Prashant-3103@users.noreply.github.com>
2022-12-31 19:50:41 +0530

    Update prime_numbers.rb

--- week_2/prime_numbers.rb
+++ week_2/prime_numbers.rb
@@ -1,18 +1,16 @@
-# A prime number is a whole number greater than 1
-# that cannot be exactly divided by any whole number 
-# other than itself and 1 
-# (e.g. 2, 3, 5, 7, 11).
-
-# The function below takes a keyword arguments `n` and 
-# returns an array of prime numbers less than or equal to
-# `n`.
-
-# For example, prime_numbers(n: 20) should return the following:
-# [2, 3, 5, 7, 11, 13, 17, 19]
-
-# If the user gives a invalid input like -4
-# We will raise an `ArgumentError` exception to let the caller know that
-# their function arguments were incorrect.
 def prime_numbers(n:)
-  raise NotImplementedError # TODO
+  raise ArgumentError, "n must be greater than or equal to 2" if n < 2
+
+  primes = []
+  (2..n).each do |i|
+    is_prime = true
+    (2..Math.sqrt(i).ceil).each do |j|
+      if i % j == 0
+        is_prime = false
+        break
+      end
+    end
+    primes << i if is_prime
+  end
+ puts primes
 end
