d56a6b37d7ebe943df75c12ef8ff0a86ebc9e3f5
Prashant-3103 <prashantkumarchoudhary.211mn035@nitk.edu.in>
2023-01-08 23:01:34 +0530

    updated my prime_number.rb file

--- week_2/prime_numbers.rb
+++ week_2/prime_numbers.rb
@@ -1,16 +1,18 @@
 def prime_numbers(n:)
-  raise ArgumentError, "n must be greater than or equal to 2" if n < 2
-
-  primes = []
-  (2..n).each do |i|
-    is_prime = true
-    (2..i-1).each do |j|
-      if i % j == 0
-        is_prime = false
-        break
-      end
-    end
-    primes << i if is_prime
-  end
- returns primes
+  if n>1
+    primes=[]
+    (2..n).each{ |n|
+      prime=true
+      (2..n/2).each{ |d|
+        if n%d==0
+          prime=false
+          break
+        end
+      }
+      primes << n if prime
+    }
+    return primes
+ else
+  raise ArgumentError
+ end
 end
