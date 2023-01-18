00cd99717e150064bf9b66bfc33637584680a36b
diviz11 <d.akkshat@gmail.com>
2023-01-18 23:48:57 +0530

    made models, ran migrations, implemented crud

--- week_3/football/app/models/football_player.rb
+++ week_3/football/app/models/football_player.rb
@@ -1,30 +1,33 @@
 class FootballPlayer < ApplicationRecord
   # select players who have postion as forward
-  scope :forwarders, -> { raise NotImplementedError }
+  scope :forwarders, -> { where(position: 'forward') }
 
   # select goalkeepers
-  scope :goalkeepers, -> { raise NotImplementedError }
+  scope :goalkeepers, -> { where(position: 'goalkeeper') }
 
   # select players from the country France
   #
   # Note - France country code is 'FRA'
-  scope :french_players, -> { raise NotImplementedError }
+  scope :french_players, -> { where(country: 'FRA') }
 
   # Sort players by their rank
-  scope :order_by_rank, -> { raise NotImplementedError }
+  scope :order_by_rank, -> { order(rank: :asc) }
 
   # Average goals per game = (total goals) / (total matches)
   #
   # If total matches or total goals is missing return nil
   def average_goals_per_game
-    raise NotImplementedError
-  end
+    return nil if !defined?(goals) || !defined?(matches_played) || matches_played == 0
+    goals.to_f / matches_played
+end
+
 
   # Total penalty cards given = red_card + yellow_card
   #
   # If red_card or yellow_card is missing return nil
   def total_penalty_cards
-    raise NotImplementedError
+    return nil if !defined?(red_card) || !defined?(yellow_card)
+    red_card + yellow_card
   end
 
   # Return the penalty succes rate of the player
@@ -32,12 +35,20 @@ class FootballPlayer < ApplicationRecord
   # 
   # if data is missing return nil
   def penalty_success_rate
-    raise NotImplementedError
+    return nil if !defined?(penalty_kicks_made) || !defined?(penalty_kicks_won)
+    (penalty_kicks_won * 100) / penalty_kicks_made
   end
 
   # create argentinian players
   def self.import_argentinian_players
-    raise NotImplementedError
+    players = [
+      ["Marcos Acuna", 13, "ARG", "defender", "Sevilla", "La Liga", 30, "1991", 2260, 105, 705, 31, 248, 81, 267, 235, 237],
+      ["Lucas Alario", 50, "ARG", "forward", "Leverkusen", "Bundesliga", 29, "1992", 655, 79, 192, 27, 193, 148, 263, 64, 172],
+      ["Sergio Aguero", 27, "ARG", "forward", "Barcelona", "La Liga", 33, "1988", 151, 179, 402, 4, 52,74, 81, 134, 146]
+    ]
+    players.each do |player|
+      FootballPlayer.create(name: player[0], rank: player[1], country: player[2], position: player[3], squad: player[4], league: player[5], age: player[6], born: player[7], minutes_played: player[8], goals: player[9], penalty_kicks_made: player[10], matches_played: player[11], corner_kicks: player[12], yellow_card: player[13], red_card: player[14], penalty_kicks_won: player[15], fouls_committed: player[16])
+    end
   end
 
   # Update the statistics of a player after a game
@@ -58,11 +69,21 @@ class FootballPlayer < ApplicationRecord
   # `ActiveRecord::RecordNotFound` exception with the player's name as
   # the message.
   def self.update_statistics(stat)
-    raise NotImplementedError
-  end
+    player = FootballPlayer.find_or_create_by(name: stat[0])
+    player.goals += stat[1]
+    player.minutes_played += stat[2]
+    player.red_card += stat[3]
+    player.yellow_card += stat[4]
+    player.save
+end
+
 
   # Delete the record associated with a player.
   def self.ban(name)
-    raise NotImplementedError
-  end
+    player = FootballPlayer.find_by(name: name)
+    if player.nil?
+        raise ActiveRecord::RecordNotFound
+    end
+    player.destroy
+end
 end
