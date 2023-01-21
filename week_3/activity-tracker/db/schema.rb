9eabf14f39f3b5e6cc6bb3a81362239767d70177
KushAnchalia <104417397+KushAnchalia@users.noreply.github.com>
2023-01-21 15:52:34 +0000

    done

--- week_3/activity-tracker/db/schema.rb
+++ week_3/activity-tracker/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_14_065748) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_16_084040) do
   create_table "activities", force: :cascade do |t|
     t.string "title"
     t.string "type"
