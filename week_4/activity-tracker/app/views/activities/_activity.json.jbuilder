ef8052b74e95947b87147e9b409466a00c886777
Parth Mittal <76661350+mittal-parth@users.noreply.github.com>
2023-01-30 03:54:23 +0000

    remove already scaffolded code

--- week_4/activity-tracker/app/views/activities/_activity.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! activity, :id, :title, :activity_type, :start, :duration, :calories, :created_at, :updated_at
-json.url activity_url(activity, format: :json)
--- week_4/activity-tracker/config/routes.rb
+++ week_4/activity-tracker/config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :activities
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
