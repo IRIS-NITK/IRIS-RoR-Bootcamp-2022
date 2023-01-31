36dc40258ce4636d4f27c8e49e6bb660ab6ca92f
Pruthvi Raaj <pruthviraaj775@oulook.com>
2023-02-01 02:08:53 +0530

    Completed Assignement 1: Activity Stats

--- week_4/activity-tracker/config/routes.rb
+++ week_4/activity-tracker/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  
+  resources :activities do
+    collection do
+      get 'stats'
+    end
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
