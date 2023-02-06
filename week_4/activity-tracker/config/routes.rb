2d0d0eb3d1b939a4cef15375bc4ac1426399dcb3
nivedphil <nived4551@gmail.com>
2023-02-06 22:47:02 +0530

    week 4 assignment 1 by Nived

--- week_4/activity-tracker/config/routes.rb
+++ week_4/activity-tracker/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resources :activities
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "activities#index"
+  get '/stats', to: 'activities#stats'
 end
