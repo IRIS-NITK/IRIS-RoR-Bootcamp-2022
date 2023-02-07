3727ee510c9fc3f500cec63ee910594a39adab4d
Parth Mittal <76661350+mittal-parth@users.noreply.github.com>
2023-02-07 15:30:44 +0530

    Week 5 Resources (#121)
    * add initial week-5 content
    
    * add readme
    
    * add readme, dark-mode and screenshots
    
    * add devise
    
    * update readme with week-5 link

---
+++ week_5/activity-tracker/app/controllers/activities_controller.rb
@@ -0,0 +1,70 @@
+class ActivitiesController < ApplicationController
+  before_action :set_activity, only: %i[ show edit update destroy ]
+
+  # GET /activities or /activities.json
+  def index
+    @activities = Activity.all
+  end
+
+  # GET /activities/1 or /activities/1.json
+  def show
+  end
+
+  # GET /activities/new
+  def new
+    @activity = Activity.new
+  end
+
+  # GET /activities/1/edit
+  def edit
+  end
+
+  # POST /activities or /activities.json
+  def create
+    @activity = Activity.new(activity_params)
+
+    respond_to do |format|
+      if @activity.save
+        format.html { redirect_to activity_url(@activity), notice: "Activity was successfully created." }
+        format.json { render :show, status: :created, location: @activity }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @activity.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /activities/1 or /activities/1.json
+  def update
+    respond_to do |format|
+      if @activity.update(activity_params)
+        format.html { redirect_to activity_url(@activity), notice: "Activity was successfully updated." }
+        format.json { render :show, status: :ok, location: @activity }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @activity.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /activities/1 or /activities/1.json
+  def destroy
+    @activity.destroy
+
+    respond_to do |format|
+      format.html { redirect_to activities_url, notice: "Activity was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_activity
+      @activity = Activity.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def activity_params
+      params.require(:activity).permit(:title, :activity_type, :start, :duration, :calories)
+    end
+end
