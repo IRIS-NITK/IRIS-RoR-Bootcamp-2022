f0aeb3f978e82727e4ef65da902810419d3ccc41
diviz11 <d.akkshat@gmail.com>
2023-02-08 23:29:49 +0530

    scaffolding and authentication added

--- week_4/activity-tracker/config/routes.rb
+++ week_4/activity-tracker/config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'static_pages#index'
+  get 'static_pages/index'
+  # get '/static_pages/index', to: 'static_pages#index'
+  get '/static_pages/about', to: 'static_pages#about'
+  resources :activities
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "activities#index"
+  get "/activities/stats", to: "activities#stats"
 end
