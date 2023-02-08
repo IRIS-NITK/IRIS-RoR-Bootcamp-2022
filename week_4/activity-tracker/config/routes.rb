bffc6845cae652d3d585ed402dc51889c97d8523
Minank-KP <107191059+Minank-KP@users.noreply.github.com>
2023-02-08 22:29:52 +0000

    week4 assignment done

--- week_4/activity-tracker/config/routes.rb
+++ week_4/activity-tracker/config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  
+  devise_for :users
+  root "static_pages#index"
+  get "about", to: "static_pages#about"
+  resources :activities do
+    collection do
+      get 'stats'
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
 end
