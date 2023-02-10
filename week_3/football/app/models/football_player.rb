320413046565970c3f1204ecedab9fccf1242d5e
ndhruv03 <ndhruva69@gmail.com>
2023-02-10 16:27:24 +0530

    Finished week 3 assignment

--- week_3/football/app/models/football_player.rb
+++ week_3/football/app/models/football_player.rb
@@ -1,30 +1,38 @@
 class FootballPlayer < ApplicationRecord
   # select players who have postion as forward
-  scope :forwarders, -> { raise NotImplementedError }
+  scope :forwarders, -> { where(position: 'forward') }
 
   # select goalkeepers
-  scope :goalkeepers, -> { raise NotImplementedError }
+  scope :goalkeepers, -> { where(position: 'goalkeeper') }
 
   # select players from the country France
   #
   # Note - France country code is 'FRA'
-  scope :french_players, -> { raise NotImplementedError }
+  scope :french_players, -> { where(country: 'FRA') }
 
   # Sort players by their rank
-  scope :order_by_rank, -> { raise NotImplementedError }
+  scope :order_by_rank, -> { order(:rank) }
 
   # Average goals per game = (total goals) / (total matches)
   #
   # If total matches or total goals is missing return nil
   def average_goals_per_game
-    raise NotImplementedError
+    if matches_played == nil || goals == nil
+      return nil
+    else
+      return goals / matches_played
+    end
   end
 
   # Total penalty cards given = red_card + yellow_card
   #
   # If red_card or yellow_card is missing return nil
   def total_penalty_cards
-    raise NotImplementedError
+    if red_card == nil || yellow_card == nil
+      return nil
+    else
+      return red_card + yellow_card
+    end
   end
 
   # Return the penalty succes rate of the player
@@ -32,12 +40,24 @@ class FootballPlayer < ApplicationRecord
   # 
   # if data is missing return nil
   def penalty_success_rate
-    raise NotImplementedError
+    if penalty_kicks_won == nil || penalty_kicks_made == nil
+      return nil
+    else
+      return (penalty_kicks_won * 100) / penalty_kicks_made
+    end
   end
 
   # create argentinian players
   def self.import_argentinian_players
-    raise NotImplementedError
+    #Acuna
+    FootballPlayer.create(name: "Marcos Acuna", rank: 13, country: "ARG", position: "defender", squad: "Sevilla", league: "La Liga", age: 30, born: 1991, minutes_played: 2260, goals: 105,
+    penalty_kicks_made: 705, matches_played: 31,corner_kicks: 248,yellow_card: 81,red_card: 267,penalty_kicks_won: 235,fouls_committed: 237)
+    #Alario
+    FootballPlayer.create(name: "Lucas Alario", rank: 50, country: "ARG", position: "forward", squad: "Leverkusen", league: "Bundesliga", age: 29, born: 1992, minutes_played: 655, goals: 79,
+    penalty_kicks_made: 192, matches_played: 27,corner_kicks: 193,yellow_card: 148,red_card: 263,penalty_kicks_won: 64,fouls_committed: 172)
+    #Aguero
+    FootballPlayer.create(name: "Sergio Aguero", rank: 27, country: "ARG", position: "forward", squad: "Barcelona", league: "La Liga", age: 33, born: 1988, minutes_played: 151, goals: 179,
+    penalty_kicks_made: 402, matches_played: 4,corner_kicks: 52,yellow_card: 74,red_card: 81,penalty_kicks_won: 134,fouls_committed: 146)
   end
 
   # Update the statistics of a player after a game
@@ -58,11 +78,26 @@ class FootballPlayer < ApplicationRecord
   # `ActiveRecord::RecordNotFound` exception with the player's name as
   # the message.
   def self.update_statistics(stat)
-    raise NotImplementedError
+    stat.each do |playerstat|
+      if FootballPlayer.find_by(name: "#{playerstat[0]}") == nil
+        raise ActiveRecord::RecordNotFound, "Player not found #{playerstat[0]}"
+      else
+        player = FootballPlayer.find_by(name: "#{playerstat[0]}")
+        player.goals += playerstat[1]
+        player.minutes_played += playerstat[2]
+        player.red_card += playerstat[3]
+        player.yellow_card += playerstat[4]
+        player.save
+      end
+    end
   end
 
   # Delete the record associated with a player.
   def self.ban(name)
-    raise NotImplementedError
+    if FootballPlayer.find_by(name: "#{name}") == nil
+      raise ActiveRecord::RecordNotFound, "Player not found #{name}"
+    else
+      FootballPlayer.destroy_by(name: "#{name}")
+    end
   end
 end
