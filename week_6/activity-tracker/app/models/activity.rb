c90cddca65ac7b5e8b7daf74267933f89dd223a7
Sharath Balaji T <sharathbalaji2003@gmail.com>
2023-02-24 19:37:46 +0000

    Added Valdation to Calories and Duration

--- week_6/activity-tracker/app/models/activity.rb
+++ week_6/activity-tracker/app/models/activity.rb
@@ -1,3 +1,11 @@
 class Activity < ApplicationRecord
     belongs_to :user
+    validates :calories, numericality: { 
+        only_integer: true, 
+        message: "only integer value"
+    }
+    validates :duration, numericality: { 
+        only_integer: true, 
+        message: "only integer value"
+    }
 end
