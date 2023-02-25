7ca8a0d615d423d0f80e6ad04588ee2adcb7e27a
ndhruv03 <ndhruva69@gmail.com>
2023-02-26 02:03:59 +0530

    Finished week assignment

--- week_4/activity-tracker/config/routes.rb
+++ week_4/activity-tracker/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :activities
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "static_pages#index"
+  get "activities/stats", to: "activities#stats", as: "stats_activities"
 end
