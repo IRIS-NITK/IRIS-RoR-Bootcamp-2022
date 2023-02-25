b16af1241cb915224611cf83cadc191dba8bf4c8
Sharath Balaji T <sharathbalaji2003@gmail.com>
2023-02-25 11:17:30 +0000

    Changed views

--- week_6/activity-tracker/config/routes.rb
+++ week_6/activity-tracker/config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   get 'static_pages/about'
   get 'about',to:"static_pages#about"
   get 'generate_pdf', to: 'activities#generate_pdf', as: 'generate_pdf_user'
-
   devise_for :users
   resources :activities do
     collection do
